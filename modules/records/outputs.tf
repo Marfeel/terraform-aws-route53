ff11a855283effd6f58c610962175f11f2a2707d
João <joao.pereira@marfeel.com>
2021-03-31 19:01:46 +0200

    Update outputs.tf

--- modules/records/outputs.tf
+++ modules/records/outputs.tf
@@ -7,3 +7,8 @@ output "this_route53_record_fqdn" {
   description = "FQDN built using the zone domain and name"
   value       = { for k, v in aws_route53_record.this : k => v.fqdn }
 }
+
+output "this_route53_records" {
+  description = "The name of the record"
+  value       = { for k, v in aws_route53_record.this : k => v.records }
+}
