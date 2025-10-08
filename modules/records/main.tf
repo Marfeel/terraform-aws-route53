6e8f336dcfc6702d0aaf1afde27915dba4219b55
Leandro Saldivar <leandro.saldivar@marfeel.com>
2025-10-08 14:11:13 -0300

    Update main.tf

--- modules/records/main.tf
+++ modules/records/main.tf
@@ -1,10 +1,16 @@
 locals {
   # convert from list to map with unique keys
-  recordsets = { for rs in var.records : join(" ", compact(["${rs.name} ${rs.type}", lookup(rs, "set_identifier", "")])) => rs }
+  recordsets = {
+    for rs in var.records :
+    join(" ", compact(["${rs.name} ${rs.type}", lookup(rs, "set_identifier", "")])) => rs
+  }
+
+  records_enabled = var.create && (var.zone_id != null || var.zone_name != null)
+  records_map     = local.records_enabled ? { for k, v in local.recordsets : k => v } : {}
 }
 
 data "aws_route53_zone" "this" {
-  count = var.create && (var.zone_id != null || var.zone_name != null) ? 1 : 0
+  count = local.records_enabled ? 1 : 0
 
   zone_id      = var.zone_id
   name         = var.zone_name
@@ -12,20 +18,19 @@ data "aws_route53_zone" "this" {
 }
 
 resource "aws_route53_record" "this" {
-  for_each = var.create && (var.zone_id != null || var.zone_name != null) ? { for k, v in local.recordsets : k => v } : {}
+  for_each = local.records_map
 
   zone_id = data.aws_route53_zone.this[0].zone_id
 
-  name           = each.value.name != "" ? "${each.value.name}.${data.aws_route53_zone.this[0].name}" : data.aws_route53_zone.this[0].name
-  type           = each.value.type
-  ttl            = lookup(each.value, "ttl", null)
-  records        = lookup(each.value, "records", null)
-  set_identifier = lookup(each.value, "set_identifier", null)
+  name            = each.value.name != "" ? "${each.value.name}.${data.aws_route53_zone.this[0].name}" : data.aws_route53_zone.this[0].name
+  type            = each.value.type
+  ttl             = lookup(each.value, "ttl", null)
+  records         = lookup(each.value, "records", null)
+  set_identifier  = lookup(each.value, "set_identifier", null)
   health_check_id = lookup(each.value, "health_check_id", null)
 
   dynamic "alias" {
     for_each = length(keys(lookup(each.value, "alias", {}))) == 0 ? [] : [true]
-
     content {
       name                   = each.value.alias.name
       zone_id                = each.value.alias.zone_id
@@ -36,17 +41,18 @@ resource "aws_route53_record" "this" {
   dynamic "geolocation_routing_policy" {
     for_each = length(keys(lookup(each.value, "geolocation_routing_policy", {}))) == 0 ? [] : [true]
     content {
-      country = each.value.geolocation_routing_policy.country
+      country   = each.value.geolocation_routing_policy.country
       continent = each.value.geolocation_routing_policy.continent
     }
   }
+
   dynamic "weighted_routing_policy" {
     for_each = length(keys(lookup(each.value, "weighted_routing_policy", {}))) == 0 ? [] : [true]
-
     content {
       weight = each.value.weighted_routing_policy.weight
     }
   }
+
   dynamic "failover_routing_policy" {
     for_each = length(keys(lookup(each.value, "failover_routing_policy", {}))) == 0 ? [] : [true]
     content {
